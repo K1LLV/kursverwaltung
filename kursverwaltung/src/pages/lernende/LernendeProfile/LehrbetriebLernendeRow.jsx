68f46d691bf779cce0bc4342601b3df3121ad0aa
K1LLV <babulia228@mail.com>
2024-02-24 20:24:23 +0100

    Implement edit lerbetriebLernende in Profile

--- kursverwaltung/src/pages/lernende/LernendeProfile/LehrbetriebLernendeRow.jsx
+++ kursverwaltung/src/pages/lernende/LernendeProfile/LehrbetriebLernendeRow.jsx
@@ -1,23 +1,95 @@
+import { useState } from "react";
 import Row from "../../../components/Row";
 import styles from "./LehrbetriebLernendeRow.module.css";
+import editStyles from "./LehrbetriebLernendeRowEdit.module.css";
+import axios from "axios";
 
 const LehrbetriebLernendeRow = props => {
 
+    const [isEdit, setIsEdit] = useState(false);
+    const [formData, setFormData] = useState(
+        {
+            beruf: props.lehrbetriebLernende.beruf,
+            start: props.lehrbetriebLernende.start,
+            ende: props.lehrbetriebLernende.ende,
+        }
+    );
+
     console.log(props.lehrbetrieb.firma);
 
-    const handleEdit = () => {};
+    const handleEdit = () => setIsEdit(x => !x);
     const handleDelete = () => {};
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(formData);
+        axios.put(`https://alex.undefiniert.ch/lehrbetrieb_lernende/${props.lehrbetriebLernende.id_lehrbetrieb_lernende}`, formData)
+            .then(() => {
+                setIsEdit(false);
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    };
 
-    return(
-        <Row
-            a={props.lehrbetrieb.firma}
-            b={props.lehrbetriebLernende.beruf}
-            c={props.lehrbetriebLernende.start}
-            d={props.lehrbetriebLernende.ende}
-            styles={styles}
-            onEdit={handleEdit}
-            onDelete={handleDelete}/>
-    );
+    const handleChange = (e) => {
+        setFormData(x => {
+            return ({...x, [e.target.name]: e.target.value})
+        });
+    };
+
+    const editForm = 
+        <form onSubmit={handleSubmit}>
+            <div className={styles.editForm}>
+                <div className={styles.berufInput}>
+                    <input
+                    className={styles.berufInput}
+                    name="beruf"
+                    type="text"
+                    defaultValue={formData.beruf}
+                    onChange={handleChange}
+                    />
+                </div>
+                
+                <div className={styles.startInput}>
+                    <input
+                    className={styles.dateInput}
+                    name="start"
+                    type="date"
+                    defaultValue={formData.start}
+                    onChange={handleChange}/>
+                </div>
+            
+                <div className={styles.endeInput}>
+                    <input
+                    className={styles.dateInput}
+                    name="ende"
+                    type="date"
+                    defaultValue={formData.ende}
+                    onChange={handleChange}/>
+                </div>
+                <button type="submit">Submit</button>
+            </div>
+        </form>
+
+    console.log(formData);
+
+    const row = isEdit
+    ?   <Row
+        a={props.lehrbetrieb.firma}
+        b={editForm}
+        styles={editStyles}
+        onEdit={handleEdit}
+        onDelete={handleDelete}/>
+    :   <Row
+        a={props.lehrbetrieb.firma}
+        b={formData.beruf}
+        c={formData.start}
+        d={formData.ende}
+        styles={styles}
+        onEdit={handleEdit}
+        onDelete={handleDelete}/>;
+
+    return(row);
 };
 
 export default LehrbetriebLernendeRow;
